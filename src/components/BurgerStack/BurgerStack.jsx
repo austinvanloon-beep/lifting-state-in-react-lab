99eb1005578626b30994dc9adda22dcbe79ca73c
Austin Vanloon <wic404learn@gmail.com>
2025-08-11 22:27:12 -0600

    lifting state react lab complete

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,17 @@
+const BurgerStack = ({ ingredients, removeFromBurger }) => {
+  return (
+    <ul>
+      {ingredients.map((ingredient, index) => (
+        <li
+          key={index}
+          style={{ backgroundColor: ingredient.color }}
+        >
+          {ingredient.name}
+          <button onClick={() => removeFromBurger(index)}>X</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default BurgerStack
